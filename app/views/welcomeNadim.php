e186b7718bd4bce27bc30842db1e024dae77f3a6
Nadimul Haque <nhsajib316@gmail.com>
2015-09-07 14:42:17 +0600

    change database,routes and more

---
+++ app/views/welcomeNadim.php
@@ -0,0 +1,17 @@
+<html>
+<title>
+    This is test
+</title>
+<body>
+<?php
+/**
+ * Created by PhpStorm.
+ * User: nadimul haque
+ * Date: 06/09/2015
+ * Time: 12:39
+ */
+echo '<button type="button" class="btn btn-primary">Primary</button>';
+echo "<p>This is a paragraph </p>";
+?>
+</body>
+</html>
